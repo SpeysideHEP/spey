7e508ada4eaa5161c53776fdfc049e9d1d7fcd8c
jackaraz <jackaraz@gmail.com>
2023-01-17 12:46:06 +0000

    new data construction TODO: slowly replace old data structure with the new one

--- src/madstats/backends/simplifiedlikelihood_backend/data.py
+++ src/madstats/backends/simplifiedlikelihood_backend/data.py
@@ -1,5 +1,149 @@
 import numpy as np
 import copy
+from dataclasses import dataclass, field
+from typing import List, Text, Union, Optional, NamedTuple
+from collections import namedtuple
+
+
+@dataclass(frozen=True)
+class DataNew:
+    """
+    Data structure for simplified likelihoods
+
+    :param observed: number of observed events.
+    :param signal: number of signal events.
+    :param background: number of expected background yields.
+    :param covariance: Covariance matrix or single region uncertainty
+    :param delta_sys: systematic uncertainty on signal.
+    :param third_moment: third moment.
+    :param name: name of the statistical model.
+
+    :raises TypeError: If the types of the inputs does not match the expected types
+    :raises AssertionError: If the dimensionality of the inputs are wrong.
+    """
+    observed: np.ndarray
+    signal: np.ndarray
+    background: np.ndarray
+    covariance: np.ndarray
+    delta_sys: Optional[float] = 0.2
+    third_moment: Optional[np.ndarray] = None
+    name: Text = "__unknown_model__"
+
+    def __post_init__(self):
+        # validate inputs
+        if not (
+            isinstance(self.observed, np.ndarray)
+            and isinstance(self.signal, np.ndarray)
+            and isinstance(self.background, np.ndarray)
+            and isinstance(self.covariance, np.ndarray)
+            and isinstance(self.delta_sys, float)
+            and (self.third_moment is None or isinstance(self.third_moment, np.ndarray))
+        ):
+            raise TypeError("Invalid type.")
+
+        assert (
+            len(self.observed)
+            == len(self.signal)
+            == len(self.background)
+            == self.covariance.shape[0]
+        ), "Input shapes does not match"
+
+        if self.third_moment is not None:
+            assert len(self.diag_cov) == len(
+                self.third_moment
+            ), "Dimensionality of the third moment does not match with covariance matrix."
+
+    @property
+    def expected_dataset(self):
+        """Retrieve expected dataset"""
+        return DataNew(
+            self.background,
+            self.signal,
+            self.background,
+            self.covariance,
+            self.delta_sys,
+            self.third_moment,
+            f"{self.name}_exp",
+        )
+
+    def __len__(self) -> int:
+        return len(self.observed)
+
+    @property
+    def var_s(self) -> np.ndarray:
+        """Variance of the signal yields"""
+        return np.diag(np.square(self.signal * self.delta_sys))
+
+    @property
+    def isAlive(self) -> bool:
+        """Is there any region with non-zero signal yields"""
+        return len(self.signal[self.signal > 0.0]) != 0
+
+    @property
+    def isLinear(self) -> bool:
+        """Is statistical model linear? i.e. no quadratic term in the poissonians"""
+        return self.third_moment is None
+
+    @property
+    def diag_cov(self) -> np.ndarray:
+        """Retrieve diagonal terms of the covariance matrix"""
+        return np.diag(self.covariance)
+
+    def compute_expansion(self) -> NamedTuple:
+        """Compute the terms described in arXiv:1809.05548 eqs. 3.10, 3.11, 3.12, 3.13"""
+        output = namedtuple("expansion", ["A", "B", "C", "rho", "V"])
+        if self.third_moment is None:
+            return output(None, None, None, None, None)
+
+        diag_cov = self.diag_cov
+
+        # arXiv:1809.05548 eq. 3.13
+        C: np.ndarray = np.zeros(shape=diag_cov.shape)
+        for idx, (m2, m3) in enumerate(zip(diag_cov, self.third_moment)):
+            m3 = m3 if m3 != 0 else 1e-30
+            k = -np.sign(m3) * np.sqrt(2.0 * m2)
+            dm = np.sqrt(8.0 * m2**3 / m3**2 - 1.0)
+            C[idx] = k * np.cos(4.0 * np.pi / 3.0 + np.arctan(dm) / 3.0)
+
+        B: np.ndarray = np.sqrt(diag_cov - 2.0 * np.square(C))  # B, as defined in Eq. 3.11
+        A: np.ndarray = self.background - C  # A, Eq. 1.30
+        rho = np.zeros(shape=(len(self), len(self)))
+        for idx in range(len(self)):
+            for idy in range(len(self)):
+                bxby = B[idx] * B[idy]
+                cxcy = C[idx] * C[idy]
+                e = (4.0 * cxcy) ** (-1) * (
+                    np.sqrt(bxby**2.0 + 8.0 * cxcy * self.covariance[idx][idy]) - bxby
+                )
+                rho[idx][idy] = e
+                rho[idy][idx] = e
+
+        V = np.zeros(shape=(len(B), len(B)))
+        for idx in range(len(B)):
+            for idy in range(idx, len(B)):
+                T = B[idx] * B[idy] * rho[idx][idy]
+                V[idx][idy] = T
+                V[idy][idx] = T
+
+        return output(A, B, C, rho, V)
+
+    @property
+    def correlation_matrix(self) -> np.ndarray:
+        """Compute correlation matrix computed from covariance matrix"""
+        corr = np.zeros(shape=self.covariance.shape)
+        for idx in range(len(self)):
+            corr[idx][idx] = 1.0
+            for idy in range(idx + 1, len(self)):
+                rho = corr[idx][idy] / np.sqrt(
+                    self.covariance[idx][idx] * self.covariance[idy][idy]
+                )
+                corr[idx][idy] = rho
+                corr[idy][idx] = rho
+        return corr
+
+    def __mul__(self, signal_strength: float) -> np.ndarray:
+        """Multiply signal yields with signal strength"""
+        return signal_strength * self.signal
 
 
 class Data:
