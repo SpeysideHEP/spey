d0d820186dc47696bf5f79f5b95db3bd772e67ad
Jack Y. Araz <jackaraz@gmail.com>
2023-03-22 13:53:30 +0100

    simplifiy interface

--- src/spey/backends/simplifiedlikelihood_backend/interface.py
+++ src/spey/backends/simplifiedlikelihood_backend/interface.py
@@ -1,6 +1,6 @@
 """Simplified Likelihood Interface"""
 
-from typing import Optional, Tuple, Text, List
+from typing import Optional, Tuple, Text, Callable
 import numpy as np
 
 from spey.optimizer import fit
@@ -57,10 +57,55 @@ class SimplifiedLikelihoodInterface(BackendBase):
             self._third_moment_expansion = self.model.compute_expansion()
         return self._third_moment_expansion
 
+    def get_twice_nll_func(
+        self,
+        expected: ExpectationType = ExpectationType.observed,
+        data: Optional[np.ndarray] = None,
+    ) -> Callable[[np.ndarray], float]:
+        """
+        Generate function to compute twice negative log-likelihood for the statistical model
+
+        :param expected (`ExpectationType`, default `ExpectationType.observed`): observed, apriori, aposteriori.
+        :param data (`Union[List[float], np.ndarray]`, default `None`): observed data to be used for nll computation.
+        :return `Callable[[np.ndarray], float]`: function to compute twice negative log-likelihood for given nuisance parameters.
+        """
+        current_model: SLData = (
+            self.model if expected != ExpectationType.apriori else self.model.expected_dataset
+        )
+        return twice_nll_func(
+            current_model.signal,
+            current_model.background,
+            data if data is not None else current_model.observed,
+            self.third_moment_expansion,
+        )
+
+    def get_gradient_twice_nll_func(
+        self,
+        expected: ExpectationType = ExpectationType.observed,
+        data: Optional[np.ndarray] = None,
+    ) -> Callable[[np.ndarray], float]:
+        """
+        Generate function to compute gradient of twice negative log-likelihood for the statistical model.
+
+        :param expected (`ExpectationType`, default `ExpectationType.observed`): observed, apriori, aposteriori.
+        :param data (`Union[List[float], np.ndarray]`, default `None`): observed data to be used for nll computation.
+        :return `Callable[[np.ndarray], float]`: function to compute twice negative log-likelihood for given nuisance parameters.
+        """
+        current_model: SLData = (
+            self.model if expected != ExpectationType.apriori else self.model.expected_dataset
+        )
+        return gradient_twice_nll_func(
+            current_model.signal,
+            current_model.background,
+            data if data is not None else current_model.observed,
+            self.third_moment_expansion,
+        )
+
     def generate_asimov_data(
         self,
-        model: SLData,
+        expected: ExpectationType = ExpectationType.observed,
         test_statistics: Text = "qtilde",
+        **kwargs,
     ) -> np.ndarray:
         """
         Generate Asimov data with respect to the given test statistics
@@ -77,6 +122,9 @@ class SimplifiedLikelihoodInterface(BackendBase):
         # fit_pars = self._asimov_nuisance.get(asimov_nuisance_key, None)
         # if fit_pars is None:
         # Generate the asimov data by fittin nuissance parameters to the observations
+        model: SLData = (
+            self.model if expected != ExpectationType.apriori else self.model.expected_dataset
+        )
 
         # Do not allow asimov data to be negative!
         par_bounds = [(0.0, 1.0)] + [
@@ -94,6 +142,7 @@ class SimplifiedLikelihoodInterface(BackendBase):
             ),
             fixed_poi_value=1.0 if test_statistics == "q0" else 0.0,
             bounds=par_bounds,
+            **kwargs,
         )
         # self._asimov_nuisance[asimov_nuisance_key] = fit_pars
 
@@ -104,8 +153,6 @@ class SimplifiedLikelihoodInterface(BackendBase):
         poi_test: Optional[float] = 1.0,
         expected: Optional[ExpectationType] = ExpectationType.observed,
         marginalize: Optional[bool] = False,
-        init_pars: Optional[List[float]] = None,
-        par_bounds: Optional[List[Tuple[float, float]]] = None,
         **kwargs,
     ) -> Tuple[float, np.ndarray]:
         """
@@ -119,167 +166,16 @@ class SimplifiedLikelihoodInterface(BackendBase):
         :param par_bounds (`Optional[List[Tuple[float, float]]]`, default `None`): bounds for fit parameters.
         :return `Tuple[float, np.ndarray]`: negative log-likelihood, fit parameters
         """
-        current_model: SLData = (
-            self.model if expected != ExpectationType.apriori else self.model.expected_dataset
-        )
 
         if marginalize:
+            current_model: SLData = (
+                self.model if expected != ExpectationType.apriori else self.model.expected_dataset
+            )
+
             nll = marginalised_negloglikelihood(
                 poi_test, current_model, self.third_moment_expansion, self.ntoys
             )
             return nll, np.nan
 
-        twice_nll, fit_param = fit(
-            func=twice_nll_func(
-                current_model.signal,
-                current_model.background,
-                current_model.observed,
-                self.third_moment_expansion,
-            ),
-            model_configuration=current_model.config(),
-            gradient=gradient_twice_nll_func(
-                current_model.signal,
-                current_model.background,
-                current_model.observed,
-                self.third_moment_expansion,
-            ),
-            initial_parameters=init_pars,
-            bounds=par_bounds,
-            fixed_poi_value=poi_test,
-            **kwargs,
-        )
-
-        return twice_nll / 2.0, fit_param
-
-    def asimov_negative_loglikelihood(
-        self,
-        poi_test: float = 1.0,
-        expected: ExpectationType = ExpectationType.observed,
-        test_statistics: Text = "qtilde",
-        init_pars: Optional[List[float]] = None,
-        par_bounds: Optional[List[Tuple[float, float]]] = None,
-        **kwargs,
-    ) -> Tuple[float, np.ndarray]:
-        """
-        compute likelihood for the Asimov data
-
-        :param poi_test (`float`, default `1.0`): parameter of interest.
-        :param expected (`ExpectationType`, default `ExpectationType.observed`): observed, apriori or aposteriori.
-        :param test_statistics (`Text`, default `"qtilde"`): test statistics: `q`, `qtilde`, `q0`.
-        :param init_pars (`Optional[List[float]]`, default `None`): initial fit parameters.
-        :param par_bounds (`Optional[List[Tuple[float, float]]]`, default `None`): bounds for fit parameters.
-        :return `Tuple[float, np.ndarray]`: negative log-likelihood, fit parameters
-        """
-        current_model: SLData = (
-            self.model if expected != ExpectationType.apriori else self.model.expected_dataset
-        )
-        data = self.generate_asimov_data(current_model, test_statistics=test_statistics)
-
-        twice_nll, fit_param = fit(
-            func=twice_nll_func(
-                current_model.signal,
-                current_model.background,
-                data,
-                self.third_moment_expansion,
-            ),
-            model_configuration=current_model.config(),
-            gradient=gradient_twice_nll_func(
-                current_model.signal,
-                current_model.background,
-                data,
-                self.third_moment_expansion,
-            ),
-            initial_parameters=init_pars,
-            bounds=par_bounds,
-            fixed_poi_value=poi_test,
-            **kwargs,
-        )
-
-        return twice_nll / 2.0, fit_param
-
-    def minimize_negative_loglikelihood(
-        self,
-        expected: ExpectationType = ExpectationType.observed,
-        allow_negative_signal: bool = True,
-        init_pars: Optional[List[float]] = None,
-        par_bounds: Optional[List[Tuple[float, float]]] = None,
-        **kwargs,
-    ) -> Tuple[float, np.ndarray]:
-        """
-        Minimize negative log-likelihood of the statistical model with respect to POI
-
-        :param expected (`Optional[ExpectationType]`, default `ExpectationType.observed`): observed, apriori or aposteriori.
-        :param allow_negative_signal (`Optional[bool]`, default `True`): if true, allow negative mu.
-        :param init_pars (`Optional[List[float]]`, default `None`): initial fit parameters.
-        :param par_bounds (`Optional[List[Tuple[float, float]]]`, default `None`): bounds for fit parameters.
-        :return `Tuple[float, np.ndarray]`: minimum negative log-likelihood and fit parameters
-        """
-        current_model: SLData = (
-            self.model if expected != ExpectationType.apriori else self.model.expected_dataset
-        )
-
-        twice_nll, fit_param = fit(
-            func=twice_nll_func(
-                current_model.signal,
-                current_model.background,
-                current_model.observed,
-                self.third_moment_expansion,
-            ),
-            model_configuration=current_model.config(allow_negative_signal=allow_negative_signal),
-            gradient=gradient_twice_nll_func(
-                current_model.signal,
-                current_model.background,
-                current_model.observed,
-                self.third_moment_expansion,
-            ),
-            initial_parameters=init_pars,
-            bounds=par_bounds,
-            **kwargs,
-        )
-
-        return twice_nll / 2.0, fit_param
-
-    def minimize_asimov_negative_loglikelihood(
-        self,
-        expected: ExpectationType = ExpectationType.observed,
-        test_statistics: Text = "qtilde",
-        init_pars: Optional[List[float]] = None,
-        par_bounds: Optional[List[Tuple[float, float]]] = None,
-        **kwargs,
-    ) -> Tuple[float, np.ndarray]:
-        """
-        Compute maximum likelihood for asimov data
-
-        :param expected (`ExpectationType`, default `ExpectationType.observed`): observed, apriori or aposteriori.
-        :param test_statistics (`Text`, default `"qtilde"`): test statistics. `"qmu"` or `"qtilde"` for exclusion
-                                     tests `"q0"` for discovery test.
-        :param init_pars (`Optional[List[float]]`, default `None`): initial fit parameters.
-        :param par_bounds (`Optional[List[Tuple[float, float]]]`, default `None`): bounds for fit parameters.
-        :return `Tuple[float, np.ndarray]`: minimum negative log-likelihood and fit parameters
-        """
-        allow_negative_signal: bool = True if test_statistics in ["q", "qmu"] else False
-        current_model: SLData = (
-            self.model if expected != ExpectationType.apriori else self.model.expected_dataset
-        )
-        data = self.generate_asimov_data(current_model, test_statistics=test_statistics)
-
-        twice_nll, fit_param = fit(
-            func=twice_nll_func(
-                current_model.signal,
-                current_model.background,
-                data,
-                self.third_moment_expansion,
-            ),
-            model_configuration=current_model.config(allow_negative_signal=allow_negative_signal),
-            gradient=gradient_twice_nll_func(
-                current_model.signal,
-                current_model.background,
-                data,
-                self.third_moment_expansion,
-            ),
-            initial_parameters=init_pars,
-            bounds=par_bounds,
-            **kwargs,
-        )
-
-        return twice_nll / 2.0, fit_param
+        # If not marginalised then use default computation method
+        raise NotImplementedError("This method has not been implemented")
