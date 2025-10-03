38e58160816557ea061c008f05de3e6a0d31f952
Jack Y. Araz <jackaraz@gmail.com>
2025-10-03 18:33:14 +0100

    validate opt options

--- src/spey/optimizer/__init__.py
+++ src/spey/optimizer/__init__.py
@@ -1,3 +1,33 @@
+import logging
+from dataclasses import dataclass
+from typing import Any
+
 from .core import fit
 
 __all__ = ["fit"]
+
+log = logging.getLogger("Spey")
+
+
+@dataclass
+class ValidateOpts:
+    """Validate optimiser options"""
+
+    opt_list: list[str]
+    must_list: list[str] = None
+    remove_list: list[str] = None
+
+    def __post_init__(self):
+        self.must_list = [] if self.must_list is None else self.must_list
+        self.remove_list = [] if self.remove_list is None else self.remove_list
+
+    def __call__(self, options: dict[str, Any]) -> dict[str, Any]:
+        if not all(k in list(options) for k in self.must_list):
+            raise AssertionError("Options should include " + ", ".join(self.must_list))
+        for key in list(options):
+            if key not in self.opt_list + self.must_list + self.remove_list:
+                log.warning("%s is not a valid option.", key)
+                options.pop(key)
+            if key in self.remove_list:
+                options.pop(key)
+        return options
