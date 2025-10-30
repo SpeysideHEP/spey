3d6d7d8e11f4c997a99595370269212e182c3c04
Jack Y. Araz <jackaraz@gmail.com>
2025-10-30 11:29:26 +0100

    update for deprecation

--- src/spey/about.py
+++ src/spey/about.py
@@ -2,13 +2,8 @@
 
 import platform
 import sys
+from importlib.metadata import version
 from subprocess import check_output
-
-import numpy
-import scipy
-import semantic_version
-import tqdm
-from pkg_resources import get_distribution, iter_entry_points
 
 
 def about() -> None:
@@ -19,22 +14,42 @@ def about() -> None:
     print(
         f"Python version:           {sys.version_info[0]}.{sys.version_info[1]}.{sys.version_info[2]}"
     )
-    print(f"Numpy version:            {numpy.__version__}")
-    print(f"Scipy version:            {scipy.__version__}")
-    print(f"Autograd version:         {get_distribution('autograd').version}")
-    print(f"tqdm version:             {tqdm.__version__}")
-    print(f"semantic_version version: {semantic_version.__version__}")
+    print(f"Numpy version:            {version('numpy')}")
+    print(f"Scipy version:            {version('scipy')}")
+    print(f"Autograd version:         {version('autograd')}")
+    print(f"tqdm version:             {version('tqdm')}")
+    print(f"semantic_version version: {version('semantic_version')}")
 
     print("\nInstalled backend plug-ins:\n")
 
     shown = ["spey"]
-    plugin_devices = iter_entry_points("spey.backend.plugins")
+    from spey import _get_entry_points
+
+    plugin_devices = _get_entry_points("spey.backend.plugins")
     for d in plugin_devices:
-        print(f"- {d.name} ({d.dist.project_name}-{d.dist.version})")
-        if d.dist.project_name not in shown:
+        if sys.version_info < (3, 10):
+            print(f"- {d.name} ({d.dist.project_name}-{d.dist.version})")
+            if d.dist.project_name not in shown:
+                print(
+                    check_output(
+                        [sys.executable, "-m", "pip", "show", d.dist.project_name]
+                    ).decode()
+                )
+                shown.append(d.dist.project_name)
+        else:
             print(
-                check_output(
-                    [sys.executable, "-m", "pip", "show", d.dist.project_name]
-                ).decode()
+                f"- {d.name} ({d.dist.metadata.json['name']}-{d.dist.metadata.json['version']})"
             )
-            shown.append(d.dist.project_name)
+            if d.dist.metadata.json["name"] not in shown:
+                print(
+                    check_output(
+                        [
+                            sys.executable,
+                            "-m",
+                            "pip",
+                            "show",
+                            d.dist.metadata.json["name"],
+                        ]
+                    ).decode()
+                )
+                shown.append(d.dist.metadata.json["name"])
