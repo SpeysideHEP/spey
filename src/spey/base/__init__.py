2e972bd45aed635a08d78416325e1edfa5711f1d
Jack Y. Araz <jackaraz@gmail.com>
2025-12-08 08:27:51 +0000

    remove unused files

--- src/spey/base/__init__.py
+++ src/spey/base/__init__.py
@@ -1,12 +1,10 @@
 from .backend_base import BackendBase, ConverterBase
 from .hypotest_base import HypothesisTestingBase
 from .model_config import ModelConfig
-from .recorder import Recorder
 
 __all__ = [
     "BackendBase",
     "HypothesisTestingBase",
     "ModelConfig",
-    "Recorder",
     "ConverterBase",
 ]
