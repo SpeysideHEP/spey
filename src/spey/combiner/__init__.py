4691af8f5286ecb7929511af9f462233be8fe6cb
Jack Y. Araz <jackaraz@gmail.com>
2026-09-21 16:07:18 +0100

    add correlated statistics combiner

--- src/spey/combiner/__init__.py
+++ src/spey/combiner/__init__.py
@@ -1,3 +1,4 @@
+from .correlated_statistics_combiner import CorrelatedStatisticsCombiner
 from .uncorrelated_statistics_combiner import UnCorrStatisticsCombiner
 
-__all__ = ["UnCorrStatisticsCombiner"]
+__all__ = ["UnCorrStatisticsCombiner", "CorrelatedStatisticsCombiner"]
